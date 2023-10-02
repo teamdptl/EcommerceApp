8c4a6582affd90fc2c5d659ef5ae2f618c31df7a
huykhaduy <huykhaduy@gmail.com>
2023-10-02 15:33:44 +0700

    update database

---
+++ client/src/layouts/Footer.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+function Footer(props) {
+    return (
+        <nav>
+            Hello world
+        </nav>
+    );
+}
+
+export default Footer;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+            <p>Content</p>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default Home;
