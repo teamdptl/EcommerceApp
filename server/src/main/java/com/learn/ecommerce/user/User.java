04a1320b9fa35d74ae5f936e2a54f56482a57b4b
huykhaduy <huykhaduy@gmail.com>
2023-10-27 13:34:46 +0700

    ngăn chặn thêm _user_seq và token_seq

--- server/src/main/java/com/learn/ecommerce/user/User.java
+++ server/src/main/java/com/learn/ecommerce/user/User.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class User implements UserDetails {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String firstname;
   private String lastname;
