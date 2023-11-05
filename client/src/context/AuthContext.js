8e4ec607efc747f5ce0e9ccd27700019a80fde48
huykhaduy <huykhaduy@gmail.com>
2023-11-05 23:27:36 +0700

    thêm auth context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -4,9 +4,13 @@ const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState({});
-    <AuthContext.Provider value={{ user, setUser }}>
-        {children}
-    </AuthContext.Provider>
+    return (
+        <>
+            <AuthContext.Provider value={{ user, setUser }}>
+                { children }
+            </AuthContext.Provider>
+        </>
+    )
 };
