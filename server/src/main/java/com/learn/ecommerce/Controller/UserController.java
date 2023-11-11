5671f22a49c991f6fecb1d3962621853374963d3
huykhaduy <huykhaduy@gmail.com>
2023-11-11 22:08:51 +0700

    Update UserController.java

--- server/src/main/java/com/learn/ecommerce/Controller/UserController.java
+++ server/src/main/java/com/learn/ecommerce/Controller/UserController.java
@@ -24,36 +24,39 @@ public class UserController {
 
     @PostMapping("/confirm-password")
     public ResponseEntity<?> changePassword(
-          @RequestBody ChangePasswordRequest request
+            @RequestBody ChangePasswordRequest request
     ) throws UnsupportedEncodingException {
-        return  service.changePassword(request);
+        return service.changePassword(request);
     }
+
     @PostMapping("/forget-password")
-    public ResponseEntity<Map<String,String>> forgotPassword(
+    public ResponseEntity<Map<String, String>> forgotPassword(
             @RequestBody String email
-    ){
+    ) {
         System.out.println(email);
-        return  service.forgotPassword(email);
+        return service.forgotPassword(email);
     }
-   // ROLE: Admin
+
+    // ROLE: Admin
     @GetMapping("/search")
     public ResponseEntity<?> getUsers(
             @RequestParam(required = false, defaultValue = "") String username,
             @RequestParam(required = false, defaultValue = "") String userId,
             @RequestParam(required = false, defaultValue = "USER") Role userRole,
-            @RequestParam(required = false, defaultValue = "false") boolean isBlocked){
+            @RequestParam(required = false, defaultValue = "false") boolean isBlocked) {
         // Trả về all user theo query
         return null;
     }
 
     // ROLE: Admin
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable String userId){
+    public ResponseEntity<?> deleteUser(@PathVariable String userId) {
         return null;
     }
 
     // ROLE: Admin và chính bản thân user
     @PutMapping("/update}")
-    public ResponseEntity<?> updateUsers(@RequestBody @Valid UpdateUserRequest request, BindingResult result){
+    public ResponseEntity<?> updateUsers(@RequestBody @Valid UpdateUserRequest request, BindingResult result) {
         return null;
-
+    }
+}
