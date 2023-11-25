b2be403b66659059ced5bd68fb7ea8aa6d7d5226
huykhaduy <huykhaduy@gmail.com>
2023-11-25 19:34:51 +0700

    fix ship info when have no user

--- server/src/main/java/com/learn/ecommerce/Request/PlaceOrderRequest.java
+++ server/src/main/java/com/learn/ecommerce/Request/PlaceOrderRequest.java
@@ -1,11 +1,16 @@
 package com.learn.ecommerce.Request;
 
 import com.learn.ecommerce.Entity.ShipInfo;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class PlaceOrderRequest {
     private PlaceOrderItem[] items;
+    @NotEmpty
     private String paymentType;
-    private CreateShipInfoRequest shipInfo;
+    @Min(0)
+    private Integer shipId;
+    // TODO: nên kiểm tra xem thằng user đó có quyền truy cập shipInfo không
 }
