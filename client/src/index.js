98fef909469eb2f189eea253cfa63dcbca42e7e0
minhtuan1108 <minhtuan.1108tn@gamil.com>
2023-11-27 23:04:35 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,7 @@ import { AuthProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
-	<AuthProvider>
 		<App/>
-	</AuthProvider>
-		
 	// </React.StrictMode>
 );
 
