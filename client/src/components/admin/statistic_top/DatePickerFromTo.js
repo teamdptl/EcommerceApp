37a9c495ffe4dce1f777875075dfd54d26c89780
Zasuz <102852013+Zasuz@users.noreply.github.com>
2023-12-07 15:50:11 +0700

    updatelam

--- client/src/components/admin/statistic_top/DatePickerFromTo.js
+++ client/src/components/admin/statistic_top/DatePickerFromTo.js
@@ -1,8 +1,17 @@
 import {Button, Datepicker, Label} from "flowbite-react";
-import React from "react";
+import React, {useState} from "react";
 import {IoIosSearch} from "react-icons/io";
 
-const DatePickerFromTo = () => {
+const DatePickerFromTo = (props) => {
+
+
+
+    const handleChangeDateFrom = (selectedDate)=>{
+        props.handleChangeDateFrom(selectedDate);
+    }
+    const handleChangeDateTo = (selectedDate) =>{
+        props.handleChangeDateTo(selectedDate);
+    }
     return (
         <>
             <div class="flex justify-between items-end gap-x-2 px-4 py-2">
@@ -10,13 +19,13 @@ const DatePickerFromTo = () => {
                     <div className="mb-2 block">
                         <Label value="Từ ngày" />
                     </div>
-                    <Datepicker language="vi-VN" labelTodayButton="Hôm nay" labelClearButton="Xóa" maxDate={new Date()}/>
+                    <Datepicker  onSelectedDateChanged={(date)=>{handleChangeDateFrom(date)}} language="vi-VN" labelTodayButton="Hôm nay" labelClearButton="Xóa" maxDate={new Date()}/>
                 </div>
                 <div>
                     <div className="mb-2 block">
                         <Label value="Đến ngày" />
                     </div>
-                    <Datepicker language="vi-VN" labelTodayButton="Hôm nay" labelClearButton="Xóa" maxDate={new Date()}/>
+                    <Datepicker onSelectedDateChanged={(date)=>{handleChangeDateTo(date)}} language="vi-VN" labelTodayButton="Hôm nay" labelClearButton="Xóa" maxDate={new Date()}/>
                 </div>
                 <Button className="mb-1" color="gray"><IoIosSearch/></Button>
             </div>
--- client/src/pages/admin/AdminStatisticTop.js
+++ client/src/pages/admin/AdminStatisticTop.js
@@ -1,12 +1,156 @@
 import {Table} from "flowbite-react";
 import DatePickerFromTo from "../../components/admin/statistic_top/DatePickerFromTo";
 import AdminPage from "../../layouts/AdminPage";
+import {useEffect, useState} from "react";
+import baseUrl from "../../config";
 
 const AdminStatisticTop = () => {
+
+
+    const [productList,setProductList] = useState([]);
+    const [brandList,setBrandList] = useState([]);
+    const [userList,setUserList] = useState([]);
+
+    const [dateFrom , setDateFrom] = useState(["","",""]);
+    const [dateTo , setDateTo] = useState(["","",""]);
+
+
+    const formatDate =(date)=>{
+        const originalDate = new Date(date);
+
+        const year = originalDate.getFullYear(); // Lấy năm
+        const month = (originalDate.getMonth() + 1).toString().padStart(2, '0'); // Lấy tháng và thêm số 0 ở đầu nếu cần
+        const day = originalDate.getDate().toString().padStart(2, '0'); // Lấy ngày và thêm số 0 ở đầu nếu cần
+
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+    }
+    const handleChangeDateFrom= (text,type)=> {
+        const updatedState = [...dateFrom];
+        switch (type){
+            case "product":
+                updatedState[0]=text
+                console.log( updatedState[0])
+                setDateFrom(updatedState);
+                break;
+            case "brand":
+                updatedState[1]=text
+                setDateFrom(updatedState);
+                break;
+            case "user":
+                updatedState[2]=text
+                setDateFrom(updatedState);
+                break;
+        }
+    }
+
+    const handleChangeDateTo= (text,type)=> {
+        const updatedState = [...dateTo];
+        switch (type){
+            case "product":
+                updatedState[0]=text
+                setDateTo(updatedState);
+                break;
+            case "brand":
+                updatedState[1]=text
+                setDateTo(updatedState);
+                break;
+            case "user":
+                updatedState[2]=text
+                setDateTo(updatedState);
+                break;
+        }
+    }
+    const fetchTopProduct =()=>{
+        let data = null;
+        if(dateFrom[0] || dateTo[0])
+        {
+             data = {
+                dateStart : dateFrom[0],
+                dateEnd : dateTo[0]
+            }
+        }
+        const option =null;
+        if (data !== null) {
+            option.body = JSON.stringify(data);
+        }
+
+        fetch(`${baseUrl}/api/v1/admin/statistic-product`,{
+            method:"POST",
+            header:{
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify(data)
+        },option)
+            .then(response =>{
+            return response.json()
+        }).then(data =>{
+            console.log(data)
+            setProductList(data)
+        })
+    }
+
+    const fetchTopBrand =()=>{
+        let data = null;
+        if(dateFrom[1] || dateTo[1])
+        {
+            data = {
+                dateStart : dateFrom[1],
+                dateEnd : dateTo[1]
+            }
+        }
+        const option =null;
+        if (data !== null) {
+            option.body = JSON.stringify(data);
+        }
+        fetch(`${baseUrl}/api/v1/admin/statistic-brand`,{
+            method:"POST",
+            header:{
+                'Content-Type': 'application/json'
+            }
+        }).then(response =>{
+            return response.json()
+        }).then(data =>{
+            console.log(data)
+            setBrandList(data)
+        })
+    }
+    const fetchTopUser =()=>{
+        let data = null;
+        if(dateFrom[2] || dateTo[2])
+        {
+            data = {
+                dateStart : dateFrom[2],
+                dateEnd : dateTo[2]
+            }
+        }
+        const option =null;
+        if (data !== null) {
+            option.body = JSON.stringify(data);
+        }
+        fetch(`${baseUrl}/api/v1/admin/statistic-user`,{
+            method:"POST",
+            header:{
+                'Content-Type': 'application/json'
+            }
+        }).then(response =>{
+            return response.json()
+        }).then(data =>{
+            console.log(data)
+            setUserList(data)
+        })
+    }
+    useEffect(() => {
+        fetchTopProduct();
+        fetchTopBrand();
+        fetchTopUser();
+    }, []);
+
     return <AdminPage>
         <div className="col-span-2 bg-white rounded-md border-2 border-zinc-100">
             <p class="text-base p-4 rounded-t-md font-semibold text-white bg-blue-500">Thống kê top 10 sản phẩm</p>
-            <DatePickerFromTo/>
+            <DatePickerFromTo handleChangeDateFrom={(text)=>handleChangeDateFrom(text,"product")}
+                              handleChangeDateTo={(text)=>handleChangeDateTo(text,"product")}/>
             <ul class="p-4 pt-2">
                 <li class="grid grid-cols-6 justify-center items-center pb-4 mb-4 border-b-2 border-b-zinc-100">
                     <div class="col-span-2">
@@ -37,7 +181,8 @@ const AdminStatisticTop = () => {
         </div>
         <div className="col-span-2 bg-white rounded-md border-2 border-zinc-100">
             <p className="text-base p-4 rounded-t-md font-semibold text-white bg-blue-500">Thống kê top 5 hãng</p>
-            <DatePickerFromTo/>
+            <DatePickerFromTo handleChangeDateFrom={(text)=>handleChangeDateFrom(text,"brand")}
+                              handleChangeDateTo={(text)=>handleChangeDateTo(text,"brand")}/>
             <ul class="p-4 pt-2">
                 <li className="grid grid-cols-6 justify-center items-center pb-4 mb-4 border-b-2 border-b-zinc-100">
                     <div className="col-span-3">
@@ -59,7 +204,8 @@ const AdminStatisticTop = () => {
         </div>
         <div className="col-span-2 bg-white rounded-md border-2 border-zinc-100">
             <p className="text-base p-4 rounded-t-md font-semibold text-white bg-blue-500">Thống kê top 5 khách hàng</p>
-            <DatePickerFromTo/>
+            <DatePickerFromTo handleChangeDateFrom={(text)=>handleChangeDateFrom(text,"user")}
+                              handleChangeDateTo={(text)=>handleChangeDateTo(text,"user")}/>
             <ul className="mt-3 p-4 pt-2">
                 <li className="grid grid-cols-6 justify-center items-center pb-4 mb-4 border-b-2 border-b-zinc-100">
                     <div className="col-span-1">
